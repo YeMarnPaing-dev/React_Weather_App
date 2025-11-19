819aa8ba59602af3bd20e96441de2a5fab4ff2ef
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-11-19 12:32:30 +0630

    FormUI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Form  from "./components/Form.jsx"
+import Card from "./components/Card.jsx"
 
 function App() {
   return (
-    <div className="App">
-     Hi This is Testing
+    <div className="w-full min-vh-100 d-flex justify-content-center align-items-center">
+     <div className='shadow-lg bg-light gap-4'>
+       <Form/>    
+      <Card/>
+     </div>
     </div>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className='w-100 d-flex justify-content-center mt-3'>
+        <input type="text" name="" id="" className='form-control mx-5' placeholder='Enter Country....'/>
+        <input type="button" value="Search" className='btn btn-primary ms-2 me-5' />
+    </div>
+  )
+}
+
+export default Form

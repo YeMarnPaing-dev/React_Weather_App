8ab32b1bd4067be5bc158f471108075fbf1c1dde
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-11-20 02:12:02 +0630

    dataSearch

--- src/App.js
+++ src/App.js
@@ -9,14 +9,19 @@ import { useEffect, useState } from 'react';
 function App() {
 const [country,setCountry] = useState('Yangon')
 const [data,setData] = useState({})  
-const fetchWeather = async () => {
-    const res = await api.get(`/weather?q=${country}&appid=${apiKey}`);
+const fetchWeather = async (country_name) => {
+ 
+  if(country_name != undefined){
+     setCountry(country_name) 
+  }
+  const res = await api.get(`/weather?q=${country}&appid=${apiKey}`);
     setData(res.data)
+ 
 };
 
 useEffect(()=> {
 fetchWeather();
-},[])
+},[country])
 
 
   
@@ -27,7 +32,7 @@ fetchWeather();
       
      <div className='shadow-lg bg-light gap-4'>
       <div className=' text-center mt-3 '>Weather App</div>  
-       <Form/>    
+       <Form fetchWeather={fetchWeather}/>    
       <Card data={data}/>
      </div>
     </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-const Form = () => {
+const Form = ({fetchWeather}) => {
+  const searchKey = useRef('')
   return (
     <div className='w-100 d-flex justify-content-center mt-3'>
-        <input type="text" name="" id="" className='form-control mx-5' placeholder='Enter Country....'/>
-        <input type="button" value="Search" className='btn btn-primary ms-2 me-5' />
+        <input type="text" ref={searchKey} name="" id="" className='form-control mx-5' placeholder='Enter Country....'/>
+        <input type="button" onClick={()=> fetchWeather(searchKey.current.value)} value="Search" className='btn btn-primary ms-2 me-5' />
     </div>
   )
 }
